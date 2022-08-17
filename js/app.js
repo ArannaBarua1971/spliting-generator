acac287507b24f9b67e96beebde7ae5a23aa9382
Aranna Barua <105977741+ArannaBarua1971@users.noreply.github.com>
2022-08-18 00:27:55 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,13 +4,12 @@ document.spliter.addEventListener('submit',function(e){
     var mainText=document.spliter.sentence.value
     var separate=document.spliter.separater.value
     var output=mainText.split(separate)
-    var breaker='<br>'
     
     for(var i=0;i<output.length ;i++){
         $('#output').append((i+1)+'. '+output[i] +'<br>')
     }
-    $("#refresh").on("click',function(){
-                    location.replace('https://arannabarua1971.github.io/spliting-generator/')
-        })
+    $('#refresh').on('click',function(){
+        location.replace('https://arannabarua1971.github.io/spliting-generator/')
+    })
 
 })
